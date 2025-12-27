9337db10e8546b8bfffcb58fe6f6716e89f773ce
Ivelin Zhelezchev <ivelin.g.zhelezchev@gmail.com>
2025-12-27 10:31:54 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const videos = document.querySelectorAll("video");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      const video = entry.target;
+      if (entry.isIntersecting) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+  },
+  { threshold: 0.6 }
+);
+
+videos.forEach(video => observer.observe(video));
